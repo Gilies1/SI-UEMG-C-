ecffd88e2fc39382e90d15578d25346f2f6c8bd7
Giliel Santos <gilielsouza@gmail.com>
2021-07-31 19:29:37 -0300

    Versão 1.0

--- Arquivo.cpp
+++ Arquivo.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
-    
+    int nota1,nota2,media;
+
+    cout <<"digite sua nota da prova 1: ";
+     scanf ("%d", &nota1);
+
+    cout <<"digite a nota da prova 2: ";
+     scanf ("%d", &nota2);
+
+       media=(nota1+nota2)/2;
+
+     cout <<"sua media e: " << media;
 }
